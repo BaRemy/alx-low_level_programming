6b3f21cbeb89bff3cbdbebadb91b84da05c0c487
BaRemy <bahatiremy05@gmail.com>
2022-12-08 17:09:45 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stio.h>
+/**
+ * main - Entry main
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char %lu byte(s)\n", sizeof(char));
+	printf("size of int %lu byte(s)\n", sizeof(int));I
+        printf("size of a long int %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float %lu byte(s)\n", sizeof(float));
+	return (0);	
+}
