aeb27b3c463afadc05a0f500eaeb28fd9ab82382
BaRemy <bahatiremy05@gmail.com>
2022-12-09 10:33:18 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -22,5 +22,5 @@ int main(void)
 			d++;
 		}
 	putchar('\n');
-	return (0);	
+	return (0);
 }
