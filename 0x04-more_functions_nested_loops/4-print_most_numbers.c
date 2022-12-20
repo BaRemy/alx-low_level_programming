7fac1344c2ed9b4f5fe8ec05f4efbd00e0668239
BaRemy <bahatiremy05@gmail.com>
2022-12-20 08:11:34 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,13 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number  = 48; numer < 58; number++)
+	for (number  = 48; number < 58; number++)
 	{
 	if ((number == 50) || (number == 52))
 	{
 	continue;
 	}
-	_putchar(number);
+	putchar(number);
 	}
-	_putchar(10);
+	putchar(10);
 }
