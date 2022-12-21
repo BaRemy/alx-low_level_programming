80b247cbbb98073ed153e5eecbbfbdaef84ae76b
BaRemy <bahatiremy05@gmail.com>
2022-12-21 03:51:58 -0800

    101-print_number

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int print_numbers(void);
+void print_number(int n);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
