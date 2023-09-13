4ed42a4a9e3b8db8b52d4c47c6091d18d922000e
BaRemy <bahatiremy05@gmail.com>
2023-09-12 17:52:34 -0700

    Create header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * @c: Character to print to standard output
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar");
-	printf("\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
