9d11572717a629d8b09a388f6e3644ec8cfd3755
BaRemy <bahatiremy05@gmail.com>
2023-10-09 17:03:38 -0700

    Add prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,5 +13,11 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
 
 #endif /*DOG_H*/
