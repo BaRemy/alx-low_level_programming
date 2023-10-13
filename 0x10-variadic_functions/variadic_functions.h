ed76921c02bd1b6a5bcf98f7e29548e48b2f3f31
BaRemy <bahatiremy05@gmail.com>
2023-10-12 19:37:10 -0700

    fix: no description(variables&functions)

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,10 +5,14 @@
 #include <stdio.h>
 
 /**
- *  * File: main
- *   *
- *    * Desc: header file
- *    **/
+ * sum_them_all - main
+ * @n: integer
+ *
+ * Return: 0
+ *
+ * Desc: header file
+ */
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
